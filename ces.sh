6f25c9bfa84cc0a1ed56c62a0da66a04cbfa0d1c
e5sub <1040155@qq.com>
2022-04-20 16:45:53 +0800

    Update ces.sh

--- ces.sh
+++ ces.sh
@@ -6,17 +6,11 @@ sys_install(){
     else
         echo 'wget 已安装，继续操作'
     fi
-	if ! type curl >/dev/null 2>&1; then
-        echo 'curl 未安装 正在安装中';
-	    apt-get install curl -y || yum install curl -y
-    else
-        echo 'curl 已安装，继续操作'
-    fi
-	if ! type docker >/dev/null 2>&1; then
-    echo 'curl 未安装 正在安装中';
+	if ! type Docker >/dev/null 2>&1; then
+    echo 'Docker 未安装 正在安装中';
 	curl -sSL https://get.docker.com/ | sh && systemctl enable docker && systemctl start docker
     else 
-        echo 'docker 已安装，继续操作'
+        echo 'Docker 已安装，继续操作'
     fi
 }
 #脚本启动
