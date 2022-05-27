565ba07e05c98b9681276b7fb30f7f2498f861d3
e5sub <1040155@qq.com>
2022-05-27 17:08:48 +0800

    新增4.36配置文件修改（测试）
    新增4.36配置文件修改（测试）

---
+++ install/4.36/ServiceConfig.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo -e "# ******************************************************"
+echo -e "#                                                      "*
+echo -e "# *脚本更新时间：2022年5月27日                         "*
+echo -e "#                                                      "*
+echo -e "# *请按照提示填写相应的参数                            "* 
+echo -e "#                                                      "*
+echo -e "# *如有不明白选项可以保持默认                          "*
+echo -e "#                                                      "*
+echo -e "# *如有问题或者遗漏的参数信息，请及时反馈              "*
+echo -e "                                                       "
+echo -e "# ******************************************************"
+echo -e "                                                       "
+# Pre-installation settings
+pre_install_ServiceConfig(){
+    # Set ces_ip
+    read -ep "(请输入CES服务器IP):" ces_ip
+    [ -z "${ces_ip}" ]
+    echo
+    echo "---------------------------"
+    echo "CES服务器IP = ${ces_ip}"
+    echo "---------------------------"
+    echo
+	# Set fsp_ip
+    read -ep "(请输入FSP服务器IP):" fsp_ip
+    [ -z "${fsp_ip}" ] 
+    echo
+    echo "---------------------------"
+    echo "FSP服务器IP = ${fsp_ip}"
+    echo "---------------------------"
+    echo
+	# Set live-fb_ip
+    read -ep "(请输入live-fb服务器IP，如没有请留空):" live_ip
+    [ -z "${live_ip}" ] && live_ip=127.0.0.1
+    echo
+    echo "---------------------------"
+    echo "live-fb服务器IP = ${live_ip}"
+    echo "---------------------------"
+    echo
+    # Set UserId
+    read -ep "(请输入开发者账号id):" UserId
+    [ -z "${UserId}" ] 
+    echo
+    echo "---------------------------"
+    echo "开发者账号id = ${UserId}"
+    echo "---------------------------"
+    echo
+    # Set SecretKey
+    read -ep "(请输入开发者密钥):" SecretKey
+    [ -z "${SecretKey}" ] 
+    echo
+    echo "---------------------------"
+    echo "开发者密钥 = ${SecretKey}"
+    echo "---------------------------"
+    echo
+	# Set AppId
+    read -ep "(请输入开发者创建的应用id):" AppId
+    [ -z "${AppId}" ] 
+    echo
+    echo "---------------------------"
+    echo "开发者创建的应用id = ${AppId}"
+    echo "---------------------------"
+    echo
+	# Set IsUseFspWbSrv
+    read -ep "(是否使用fsp白板服务:0不使用):" IsUseFspWbSrv
+    [ -z "${IsUseFspWbSrv}" ] && IsUseFspWbSrv=1
+    echo
+    echo "---------------------------"
+    echo "使用fsp白板服务 = ${IsUseFspWbSrv}"
+    echo "---------------------------"
+    echo
+	# Set FspDomain
+    read -ep "(请输入fsp域，不填则默认pri):" FspDomain
+    [ -z "${FspDomain}" ] && FspDomain=pri
+    echo
+    echo "---------------------------"
+    echo "FSP域 = ${FspDomain}"
+    echo "---------------------------"
+    echo
+}
+
+# Config ServiceConfig
+config_ServiceConfig(){
+    echo "正在写入FMserver配置文件"	
+    sed -i "s|app-id.*|app-id: ${AppId}|"  /usr/local/hst/FMServer/ServiceConfig.xml
+    sed -i "s|<UserId>.*|<UserId>${UserId}</UserId>|"  /usr/local/hst/FMServer/ServiceConfig.xml
+    sed -i "s|<SecretKey>.*|<SecretKey>${<SecretKey>}</<SecretKey>>|"  /usr/local/hst/FMServer/ServiceConfig.xml
+    sed -i "s|<AppId>.*|<AppId>${AppId}</<AppId>|"  /usr/local/hst/FMServer/ServiceConfig.xml
+	sed -i "s|<FspAccessAddr>.*|<FspAccessAddr>http://${fsp_ip}:20020/server/address</FspAccessAddr>|"  /usr/local/hst/FMServer/ServiceConfig.xml
+    sed -i "s|<FspDomain>.*|<FspDomain>${FspDomain}</FspDomain>|" /usr/local/hst/FMServer/ServiceConfig.xml    
+	sed -i "s|<IsUseFspWbSrv>.*|<IsUseFspWbSrv>${IsUseFspWbSrv}</IsUseFspWbSrv>|" /usr/local/hst/FMServer/ServiceConfig.xml    
+	sed -i "s|Ice.Default.Locator.*|Ice.Default.Locator = LiveServiceIceGrid/Locator:ssl -h ${live_ip} -p 10000|" /usr/local/hst/FMServer/live_ice.cfg
+	echo "写入成功，正在重启FMservice服务，请耐心等待"
+}
+
+pre_install_ServiceConfig
+config_ServiceConfig
+
+#重启FMservice服务
+service fmservice restart
