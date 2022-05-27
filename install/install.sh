8d92c117f2ebd2fa3f1283aa827c5440f30fad83
e5sub <1040155@qq.com>
2022-05-27 09:18:40 +0800

    修改端口，防止跟CES冲突
    修改端口，防止跟CES冲突

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 echo -e "# ******************************************************"
 echo -e "#                                                      "*
-echo -e "# *脚本更新时间：2022年5月23日                         "*
+echo -e "# *脚本更新时间：2022年5月27日                         "*
 echo -e "#                                                      "*
 echo -e "# *正在执行所选择的项目，请耐心等待                    "* 
 echo -e "#                                                      "*
@@ -836,7 +836,7 @@ then
 	echo -e "\033[33m 【你选择的是安装RTMP/WebRTC/HLS/HTTP-FLV/SRT实时视频服务器】 \033[0m"
 	echo -e "\n"
 	sleep 5s
-	docker run -d --restart=unless-stopped -p 1935:1935 -p 1985:1985 -p 8080:8080 -p 1990:1990 -p 8088:8088 --env CANDIDATE="${LOCAL_IP}" -p 8000:8000/udp --name srs ossrs/srs:4 ./objs/srs -c conf/https.docker.conf
+	docker run -d --restart=unless-stopped -p 1935:1935 -p 1985:1985 -p 8081:8080 -p 1990:1990 -p 8088:8088 --env CANDIDATE="${LOCAL_IP}" -p 8000:8000/udp --name srs ossrs/srs:4 ./objs/srs -c conf/https.docker.conf
 fi
 if [ $1 = '-iperf' ]
 then
