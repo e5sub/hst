dbcafd2fb837554cd41f00d215edf9b56043a893
e5sub <1040155@qq.com>
2022-09-02 20:01:52 +0800

    Update ces.sh

--- ces.sh
+++ ces.sh
@@ -22,7 +22,7 @@ sys_install(){
     fi
 }
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-sh_ver="1.0.5"
+sh_ver="1.0.6"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 github="ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master"
 #更新脚本
