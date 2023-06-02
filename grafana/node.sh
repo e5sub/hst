cd6373502de7097e750d5dc86a3f37d0a1d8e3a7
e5sub <46080660+e5sub@users.noreply.github.com>
2023-06-02 18:10:07 +0800

    添加node安装

---
+++ grafana/node.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo -e "                                                       "
+echo -e "# ******************************************************"
+echo -e "#                                                      "*
+echo -e "# *脚本更新时间：2023年6月2日                          "*
+echo -e "#                                                      "*
+echo -e "# *抖音、微信视频号：萌萌哒菜芽，欢迎关注！            "*
+echo -e "#                                                      "*
+echo -e "# ******************************************************"
+echo -e "                                                       "
+# 安装docker和docker-compose
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun | systemctl enable docker && systemctl start docker
+pip3 install -upgrade pip && pip3 install docker-compose
+# 使用docker部署node-exporter
+docker run -d -p 9100:9100 -v /home/grafana/node-exporter/proc:/host/proc:ro -v /home/grafana/node-exporter/sys:/host/sys:ro -v /home/grafana/node-exporter/:/homefs:ro --name node-exporter --restart=always prom/node-exporter
+echo -e "                                                                                "
+echo -e "#*******************************************************************************"
+echo -e "#                                                                               "
+echo -e "# *登陆信息                                                                     "
+echo -e "#                                                                               "
+echo -e "# *node-exporter: http://$local_ip:9100/metrics                                 "
+echo -e "#                                                                               "
+echo -e "# *模板下载地址 https://grafana.com/api/dashboards/8919/revisions/25/download   "
+echo -e "#                                                                               "
+echo -e "# ******************************************************************************"
+echo -e "                                                                                "
