5c05a02f67bf9950c498a745508c33c4942af6bd
e5sub <46080660+e5sub@users.noreply.github.com>
2023-06-03 16:26:48 +0800

    fix

--- ces.sh
+++ ces.sh
@@ -143,38 +143,38 @@ echo -e " \033[32m 3. \033[0m CES v4.35.4.5服务器"
 echo -e " \033[31m=====*4.34版本*=====\033[0m"
 echo -e " \033[32m 4. \033[0m CES v4.34.5.1服务器"
 echo -e " \033[44;37m 安装FSP服务器 \033[0m"
-echo -e " \033[32m 60. \033[0m 安装FSP v1.7.5.1服务器（配套4.34、4.35服务器）"
-echo -e " \033[32m 61. \033[0m 安装FSP v1.8.3.5服务器（配套4.36服务器）"
+echo -e " \033[32m 5. \033[0m 安装FSP v1.7.5.1服务器（配套4.34、4.35服务器）"
+echo -e " \033[32m 6. \033[0m 安装FSP v1.8.3.5服务器（配套4.36服务器）"
 echo -e " \033[44;37m 安装H323服务器 \033[0m"
-echo -e " \033[32m 70. \033[0m 安装H323网关服务器v2.4.3.2（私有云版本）"
-echo -e " \033[32m 71. \033[0m 安装H323网关服务器v2.4.3.2（云会议版本）"
+echo -e " \033[32m 7. \033[0m 安装H323网关服务器v2.4.3.2（私有云版本）"
+echo -e " \033[32m 8. \033[0m 安装H323网关服务器v2.4.3.2（云会议版本）"
 echo -e " \033[44;37m 安装录制服务器 \033[0m"
-echo -e " \033[32m 75. \033[0m 安装录制服务器v3.2.6.17（适用于CES V4.36版本，不能与FSP安装在同一台）"
+echo -e " \033[32m 9. \033[0m 安装录制服务器v3.2.6.17（适用于CES V4.36版本，不能与FSP安装在同一台）"
 echo -e " \033[44;37m 其他（非好视通产品） \033[0m"
-echo -e " \033[32m 80. \033[0m 安装RTMP/WebRTC/HLS/HTTP-FLV/SRT实时视频服务器（1935/1985/1990/8000/8081/8088端口）"
-echo -e " \033[32m 81. \033[0m 安装iperf3网络性能测试工具(服务端)（5201端口）"
-echo -e " \033[32m 82. \033[0m 安装HTML5网络速度测试工具(服务端)（6688端口）"
-echo -e " \033[32m 83. \033[0m 安装动态域名解析服务（浏览器打开主机IP:9876）"
-echo -e " \033[32m 84. \033[0m 网络同步服务器时间（需要服务器能连接公网）"
-echo -e " \033[32m 85. \033[0m 安装RustDesk远程桌面服务器（21115/21116/21117/21118/21119端口）"
-echo -e " \033[32m 86. \033[0m 安装Frp内网穿透服务器（配置文件存放路径/frp/frps.ini）"
-echo -e " \033[32m 87. \033[0m 安装Frp内网穿透客户端（配置文件存放路径/frp/frpc.ini）"
-echo -e " \033[32m 88. \033[0m 安装Prometheus+Grafana+node-exporter+consul"
-echo -e " \033[32m 880. \033[0m 安装node-exporter"
+echo -e " \033[32m 10. \033[0m 安装RTMP/WebRTC/HLS/HTTP-FLV/SRT实时视频服务器（1935/1985/1990/8000/8081/8088端口）"
+echo -e " \033[32m 11. \033[0m 安装iperf3网络性能测试工具(服务端)（5201端口）"
+echo -e " \033[32m 12. \033[0m 安装HTML5网络速度测试工具(服务端)（6688端口）"
+echo -e " \033[32m 13. \033[0m 安装动态域名解析服务（浏览器打开主机IP:9876）"
+echo -e " \033[32m 14. \033[0m 网络同步服务器时间（需要服务器能连接公网）"
+echo -e " \033[32m 15. \033[0m 安装RustDesk远程桌面服务器（21115/21116/21117/21118/21119端口）"
+echo -e " \033[32m 16. \033[0m 安装Frp内网穿透服务器（配置文件存放路径/frp/frps.ini）"
+echo -e " \033[32m 17. \033[0m 安装Frp内网穿透客户端（配置文件存放路径/frp/frpc.ini）"
+echo -e " \033[32m 18. \033[0m 安装Prometheus+Grafana+node-exporter+consul"
+echo -e " \033[32m 19. \033[0m 安装node-exporter"
 echo -e ""
-echo -e " \033[32m 89. \033[0m 卸载CES服务器"
-echo -e " \033[32m 90. \033[0m 卸载FSP服务器"
+echo -e " \033[32m 88. \033[0m 卸载CES服务器"
+echo -e " \033[32m 89. \033[0m 卸载FSP服务器"
 echo -e ""
-echo -e " \033[32m 91. \033[0m 修改H323服务器配置信息（适用于2.4.2.2私有云版本）"
-echo -e " \033[32m 92. \033[0m 修改H323服务器配置信息（适用于2.4.2.2云会议版本）"
-echo -e " \033[32m 93. \033[0m 修改CES V4.36 配置信息（需先安装FSP服务器）"
-echo -e " \033[32m 94. \033[0m 修改节点服务器配置信息（主服务器勿用）"
-echo -e " \033[32m 95. \033[0m Nginx转发8443端口，隐藏公网8443后台页面（转发端口号18443）"
-echo -e " \033[32m 96. \033[0m Nginx反代1089/8443/20020端口（需手动修改/usr/local/nginx/nginx.conf里的地址）"
-echo -e " \033[32m 97. \033[0m 放行服务器所有端口"
+echo -e " \033[32m 90. \033[0m 修改H323服务器配置信息（适用于2.4.2.2私有云版本）"
+echo -e " \033[32m 91. \033[0m 修改H323服务器配置信息（适用于2.4.2.2云会议版本）"
+echo -e " \033[32m 92. \033[0m 修改CES V4.36 配置信息（需先安装FSP服务器）"
+echo -e " \033[32m 93. \033[0m 修改节点服务器配置信息（主服务器勿用）"
+echo -e " \033[32m 94. \033[0m Nginx转发8443端口，隐藏公网8443后台页面（转发端口号18443）"
+echo -e " \033[32m 95. \033[0m Nginx反代1089/8443/20020端口（需手动修改/usr/local/nginx/nginx.conf里的地址）"
+echo -e " \033[32m 96. \033[0m 放行服务器所有端口"
 echo -e ""
-echo -e " \033[32m 98. \033[0m 重置后台admin密码"
-echo -e " \033[32m 99. \033[0m 安装CES历史版本以及国产化版本"
+echo -e " \033[32m 97. \033[0m 重置后台admin密码"
+echo -e " \033[32m 98. \033[0m 安装CES历史版本以及国产化版本"
 echo -e " \033[32m 00. \033[0m 安装中性版服务器"
 echo -e ""
 echo && read -ep "请输入数字选择: " N
@@ -185,35 +185,35 @@ case $N in
   2) bash install.sh -436 ;;
   3) bash install.sh -435 ;;
   4) bash install.sh -434 ;;
-  60) bash install.sh -175fsp ;;
-  61) bash install.sh -183fsp ;;
-  70) bash install.sh -h323pri ;;
-  71) bash install.sh -h323pub ;;
-  75) bash install.sh -record ;;
-  80) docker run -d --restart=always -p 1935:1935 -p 1985:1985 -p 8081:8080 -p 1990:1990 -p 8088:8088 --env CANDIDATE="${LOCAL_IP}" -p 8000:8000/udp --name srs ossrs/srs:4 ./objs/srs -c conf/https.docker.conf ;;
-  81) docker run -d --restart=always -p 5201:5201 -p 5201:5201/udp --name iperf3 ccr.ccs.tencentyun.com/1040155/iperf3 -s ;;  
-  82) docker run -d --restart=always -p 6688:80 --name hst-speedtest 1040155/hst-speedtest ;; 
-  83) docker run -d --name ddns-go --restart=always --net=host -v /opt/ddns-go:/root jeessy/ddns-go ;;
-  84) bash install.sh -time ;;
-  85) docker run --name hbbs -p 21115:21115 -p 21116:21116 -p 21116:21116/udp -p 21118:21118 -v /home/rustdesk:/root -td --restart=always rustdesk/rustdesk-server hbbs -r ${LOCAL_IP}
+  5) bash install.sh -175fsp ;;
+  6) bash install.sh -183fsp ;;
+  7) bash install.sh -h323pri ;;
+  8) bash install.sh -h323pub ;;
+  9) bash install.sh -record ;;
+  10) docker run -d --restart=always -p 1935:1935 -p 1985:1985 -p 8081:8080 -p 1990:1990 -p 8088:8088 --env CANDIDATE="${LOCAL_IP}" -p 8000:8000/udp --name srs ossrs/srs:4 ./objs/srs -c conf/https.docker.conf ;;
+  11) docker run -d --restart=always -p 5201:5201 -p 5201:5201/udp --name iperf3 ccr.ccs.tencentyun.com/1040155/iperf3 -s ;;  
+  12) docker run -d --restart=always -p 6688:80 --name hst-speedtest 1040155/hst-speedtest ;; 
+  13) docker run -d --name ddns-go --restart=always --net=host -v /opt/ddns-go:/root jeessy/ddns-go ;;
+  14) bash install.sh -time ;;
+  15) docker run --name hbbs -p 21115:21115 -p 21116:21116 -p 21116:21116/udp -p 21118:21118 -v /home/rustdesk:/root -td --restart=always rustdesk/rustdesk-server hbbs -r ${LOCAL_IP}
       docker run --name hbbr -p 21117:21117 -p 21119:21119 -v /home/rustdesk:/root -td --restart=always rustdesk/rustdesk-server hbbr  ;;
-  86) bash install.sh -frps ;;
-  87) bash install.sh -frpc ;;
-  88) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/grafana/install.sh && bash install.sh ;;
-  880) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/grafana/node.sh && bash node.sh ;;
-  89) bash install.sh -xiezai ;;
-  90) bash install.sh -unfsp ;;
-  91) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/h323pri.sh && bash h323pri.sh ;;
-  92) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/h323pub.sh && bash h323pub.sh ;;
-  93) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/4.36/config.sh && bash config.sh ;;
-  94) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/node.sh && bash node.sh ;;
-  95) bash install.sh -nginx ;;
-  96) bash install.sh -proxy ;;
-  97) systemctl stop firewalld.service
+  16) bash install.sh -frps ;;
+  17) bash install.sh -frpc ;;
+  18) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/grafana/install.sh && bash install.sh ;;
+  19) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/grafana/node.sh && bash node.sh ;;
+  88) bash install.sh -xiezai ;;
+  89) bash install.sh -unfsp ;;
+  90) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/h323pri.sh && bash h323pri.sh ;;
+  91) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/h323pub.sh && bash h323pub.sh ;;
+  92) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/4.36/config.sh && bash config.sh ;;
+  93) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/node.sh && bash node.sh ;;
+  94) bash install.sh -nginx ;;
+  95) bash install.sh -proxy ;;
+  96) systemctl stop firewalld.service
       systemctl disable firewalld.service ;;
 
-  98) bash install.sh -resetadmin ;;
-  99) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/old.sh && bash old.sh ;;
+  97) bash install.sh -resetadmin ;;
+  98) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/old.sh && bash old.sh ;;
   00) wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master/install/zxces.sh && bash zxces.sh ;;
   *)
       clear
