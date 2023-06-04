d3faef677785143f05b7856b7e017d8680e68a2b
e5sub <46080660+e5sub@users.noreply.github.com>
2023-06-04 19:33:48 +0800

    修改docker安装地址

--- ces.sh
+++ ces.sh
@@ -22,7 +22,7 @@ sys_install(){
     fi
     if ! type docker >/dev/null 2>&1; then
         echo 'docker 未安装 正在安装中';
-        curl -sSL https://get.docker.com/ | sh  | systemctl enable docker && systemctl start docker
+        curl -sSL https://get.daocloud.io/docker/ | sh  | systemctl enable docker && systemctl start docker
     else 
         echo 'docker 已安装，继续操作'
     fi
