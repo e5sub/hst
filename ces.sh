46f8884c72fb517b447e9def51c1477e3576312d
e5sub <46080660+e5sub@users.noreply.github.com>
2023-07-03 03:14:15 +0000

    Update ces.sh

--- ces.sh
+++ ces.sh
@@ -195,7 +195,7 @@ case $N in
   11) docker run -dit --restart=always -p 2022:2022 -p 1935:1935/tcp -p 1985:1985/tcp -p 8080:8080/tcp -p 8000:8000/udp -p 10080:10080/udp --name srs-cloud -v /home/ossrs/db:/data ossrs/srs-cloud:platform-1
   12) docker run -dit --restart=always -p 5201:5201 -p 5201:5201/udp --name iperf3 ccr.ccs.tencentyun.com/1040155/iperf3 -s ;;  
   13) docker run -dit --restart=always -p 6688:80 --name hst-speedtest 1040155/hst-speedtest ;; 
-  14) docker run -dit --name ddns-go --restart=always --net=host -v /opt/ddns-go:/root jeessy/ddns-go ;;
+  14) docker run -dit --name ddns-go --restart=always --net=host -v /home/ddns-go:/root jeessy/ddns-go ;;
   15) bash install.sh -time ;;
   16) docker run --name hbbs -p 21115:21115 -p 21116:21116 -p 21116:21116/udp -p 21118:21118 -v /home/rustdesk:/root -dit --restart=always rustdesk/rustdesk-server hbbs -r ${LOCAL_IP}
       docker run --name hbbr -p 21117:21117 -p 21119:21119 -v /home/rustdesk:/root -dit --restart=always rustdesk/rustdesk-server hbbr  ;;
