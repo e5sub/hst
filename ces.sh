cfb64715339657abed02559d7b7891ba3f818622
Classie Beahan <46080660+e5sub@users.noreply.github.com>
2023-08-14 21:47:38 +0800

    fix bug

--- ces.sh
+++ ces.sh
@@ -31,7 +31,7 @@ sys_install(){
 }
 sys_install
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-sh_ver="1.6"
+sh_ver="2.0"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 github="ghproxy.com/https://raw.githubusercontent.com/e5sub/hst/master"
 #更新脚本
