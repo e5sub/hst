786d9e2876422749dfc87c15edb74e58ac5e0bf9
菜芽 <46080660+e5sub@users.noreply.github.com>
2024-05-17 16:05:55 +0800

    del

--- install/wx/init/consumer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 75s
-#chkconfig:2345 61 61
-#启动进程
-JAVA_OPT="-Xms256m -Xmx512m --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.math=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED --add-opens java.rmi/sun.rmi.transport=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED"
-
-nohup /opt/jdk17/bin/java  -jar $JAVA_OPT /app/java/smart-park-consumer-boot.jar >/app/java/smart-park-consumer-boot`date +%Y%m%d`.log 2>&1 &
